733e5ec19ed79b10d9118bd6106780f4279c0fa5
Harsh Sood <76679188+harshsood@users.noreply.github.com>
2025-09-24 11:01:48 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import DeliveryAreas from '../components/DeliveryAreas';
-import Certifications from 'src/components/Certifications.tsx';
+import Certifications from "../../components/Certifications";
 
 const Home = () => {
   return (
